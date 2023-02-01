158b7a54be32b18498401ff17189062469841e04
Sameer Negi <sameer.negi17@gmail.com>
2023-02-01 20:15:24 +0100

    files python

--- Data_structures_and_algo/python_files/keep_last_n_terms.py
+++ Data_structures_and_algo/python_files/keep_last_n_terms.py
@@ -6,17 +6,12 @@ some other kind of processing.
 
 from collections import deque
 
-def search(lines, pattern, history=5):
-    previous_lines = deque(maxlen=history)
-    for line in lines:
-        if pattern in line:
-            yield line,previous_lines
-        previous_lines.append(line)
+history = deque(maxlen=5)
 
-if __name__ == "__main__":
-    with open("data\somefile.txt") as f:
-        for line, prevlines in search(f,"python",5):
-            for pline in prevlines:
-                print(pline,end="")
-            print(line,end="")
-            print("-----------------------------")
+
+
+with open("data/somefile.txt") as f:
+    for line in f:
+        history.append(line)
+        print(history)
+        print("------------------------------")
